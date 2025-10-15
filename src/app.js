a23dffa3ffec4e7a5d8b86b3136c09abf029a118
RAGNAR303 <goncalves303.gt@gmail.com>
2025-10-15 15:36:02 -0300

    add cors import

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import 'dotenv/config.js';
+import cors from 'cors';
 import express from 'express';
 import fileRoutesConfig from './config/fileRoutes.cjs';
 import routes from './routes.js';
@@ -7,6 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use('/product-file', fileRoutesConfig);
 app.use('/category-file', fileRoutesConfig);
 app.use(routes);
