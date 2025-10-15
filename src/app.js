c85d342ddd90adadaf0775a8e0631a37d6a24a7f
RAGNAR303 <goncalves303.gt@gmail.com>
2025-10-15 17:15:40 -0300

    edit enc , add cors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,11 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+  }),
+);
 app.use('/product-file', fileRoutesConfig);
 app.use('/category-file', fileRoutesConfig);
 app.use(routes);
